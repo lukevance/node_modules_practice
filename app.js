6b404287f223844b3b93b2400afaff31e2e48c98
Luke Vance <lukevance90@gmail.com>
2015-12-08 13:32:03 -0700

    finished exercise

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //This is where you should incorporate your modules and execute your app
+var response = require('./autoResponse.js');
+var fs = require('fs');
+var weekday = require('weekday');
+
+var autoRespond = response();
+
+var today = weekday(weekday() + 1);
+//
+var message = autoRespond.getResponse(today);
+
+fs.appendFile('message.txt', message, function (err) {
+  if (err) throw err;
+  console.log(message + ' was appended to file!');
+});
--- autoResponse.js
+++ autoResponse.js
@@ -1,1 +1,24 @@
 //Write your Constructor Function that will generate a different response for each day of the week
+var Response = function(){
+  var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  var messages = [
+    'Welcome to the Monday Morning blues, at least there is football tonight.',
+    'Ruby Tuesday, the week is just getting started.',
+    'Wednesday! Humpday, halfway through the week!',
+    'Thursday is not so bad, just one more day till the weekend!',
+    "It's, Friday, it's Friday, gotta get down on Friday!",
+    'Saturday. Keep sleeping.',
+    'It is Sunday! Get ready for next week!'
+  ];
+  return {
+    getResponse: function(day){
+      for (var i = 0; i < days.length; i++){
+        if (day === days[i]){
+          return messages[i];
+        }
+      }
+    }
+  };
+};
+
+module.exports = Response;
